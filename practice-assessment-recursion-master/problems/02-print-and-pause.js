cbf2ccf903243341176c57a79a4f928da275e54b
gary <baby_plant@icloud.com>
2021-03-26 16:31:21 -0700

    first commit

---
+++ practice-assessment-recursion-master/problems/02-print-and-pause.js
@@ -0,0 +1,37 @@
+/***********************************************************************
+Given a list of positive integers, print each integer and pause for that
+many milliseconds before printing the next one.
+
+Example:
+
+printAndPause([200, 800, 200, 800, 200, 800])
+200
+// pause 200ms
+800
+// pause 800ms
+200
+// pause 200ms
+800
+// pause 800ms
+200
+// pause 200ms
+800
+// pause 800ms
+
+Run tests on just this file by typing `npm test test/02-print-and-pause-spec.js`
+on the command line.
+
+***********************************************************************/
+
+function printAndPause(nums) {
+
+    // Your code here
+
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+  module.exports = printAndPause;
+} catch {
+  module.exports = null;
+}
